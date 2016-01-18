20a689232d178c1cb9d2c218660eadede10e0b9a
Fei Gao <fgao22@emory.edu>
2016-01-17 17:32:54 -0800

    Editing existing testing

--- functional_tests/test_all_users.py
+++ functional_tests/test_all_users.py
@@ -12,7 +12,6 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
-        activate('en')
 
     def tearDown(self):
         self.browser.quit()
@@ -22,7 +21,7 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
 
     def test_home_title(self):
         self.browser.get(self.get_full_url("home"))
-        self.assertIn("TaskBuster", self.browser.title)
+        self.assertIn("PathFinder", self.browser.title)
 
     def test_h1_css(self):
         self.browser.get(self.get_full_url("home"))
@@ -35,24 +34,6 @@ class HomeNewVisitorTest(StaticLiveServerTestCase):
         self.assertNotIn("Not Found", self.browser.title)
         self.browser.get(self.live_server_url + "/humans.txt")
         self.assertNotIn("Not Found", self.browser.title)
-
-    def test_internationalization(self):
-        for lang, h1_text in [('en', 'Welcome to TaskBuster!'),
-                              ('ca', 'Benvingut a TaskBuster!')]:
-            activate(lang)
-            self.browser.get(self.get_full_url("home"))
-            h1 = self.browser.find_element_by_tag_name("h1")
-            self.assertEqual(h1.text, h1_text)
-
-    def test_localization(self):
-        today = date.today()
-        for lang in ['en', 'ca']:
-            activate(lang)
-            self.browser.get(self.get_full_url("home"))
-            local_date = self.browser.find_element_by_id("local-date")
-            non_local_date = self.browser.find_element_by_id("non-local-date")
-            self.assertEqual(formats.date_format(today, use_l10n=True), local_date.text)
-            self.assertEqual(today.strftime('%Y-%m-%d'), non_local_date.text)
 
     def test_time_zone(self):
         self.browser.get(self.get_full_url("home"))
--- pathfinder/test.py
+++ pathfinder/test.py
@@ -7,11 +7,9 @@ from django.utils.translation import activate
 class TestHomePage(TestCase):
 
     def test_uses_index_template(self):
-        activate('en')
         response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "trail/index.html")
 
     def test_uses_base_template(self):
-        activate('en')
         response = self.client.get(reverse("home"))
         self.assertTemplateUsed(response, "base.html")
