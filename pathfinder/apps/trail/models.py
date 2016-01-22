f4943bb39777cc59952ae270b99840460e16d555
Fei Gao <fgao22@emory.edu>
2016-01-22 15:53:33 -0800

    Form testing finished Bug fixing

--- pathfinder/apps/trail/models.py
+++ pathfinder/apps/trail/models.py
@@ -7,15 +7,15 @@ from django.db.models.signals import post_save
 class UserProfile(models.Model):
 	user = models.OneToOneField(User)
 	date_joined = models.DateTimeField(('date_joined'), default=timezone.now)
-	
+
 	objects = UserManager()
-	
+
 	def create_user_profile(sender, instance, created, **kwargs):
 		if created:
 			UserProfile.objects.create(user=instance)
 
 	def __str__(self):
-		return self.user.username	
+		return self.user.username
 	post_save.connect(create_user_profile, sender=User)
 
 class Category(models.Model):
@@ -23,16 +23,16 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-	
+
 class Spark(models.Model):
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, unique=True)
     slug = models.SlugField(db_index=False, blank=True, unique=True)
     url = models.URLField()
     start_date = models.DateField(blank=False)
     end_date = models.DateField(blank=True,null=True)
     description = models.TextField(blank = True)
     category = models.ForeignKey(Category, default=1)
-    author = models.ForeignKey(User, null=True)
-	
+    author = models.ForeignKey(User, null=False)
+
     def __str__(self):
         return self.name
--- pathfinder/tests/test_forms.py
+++ pathfinder/tests/test_forms.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
+import datetime
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 from pathfinder.forms import *
-from pathfinder.apps.trail.models import User
+from pathfinder.apps.trail.models import User, Category, Spark
 
 class UserFormTest(TestCase):
 
     def setUp(self):
-        user = get_user_model().objects.create_user('test123')
+        pass
 
     def test_init(self):
         UserForm()
@@ -35,4 +37,69 @@ class UserFormTest(TestCase):
         self.assertEqual(form.errors, {
             'username': ['This field is required.'],
             'password': ['This field is required.'],
+        })
+
+class SparkFormTest(TestCase):
+
+    def setUp(self):
+        self.user = get_user_model().objects.create_user('test123')
+        self.category = Category.objects.create(name='Test Category')
+        self.spark = Spark.objects.create(
+            name='Test Spark Duplicate',
+            category=self.category,
+            start_date='1993-12-13',
+            end_date="2014-1-1",
+            url="http://test.com",
+            description="This is a test description",
+            author=self.user,
+        )
+
+    def test_init(self):
+        AddSpark(author=self.user,)
+
+    def test_init_without_entry(self):
+        with self.assertRaises(KeyError):
+            AddSpark()
+
+    def test_valid_data(self):
+        form = AddSpark({
+            'name': 'Test Project',
+            'category': Category.objects.filter(name="Test Category"),
+            'start_date': '1993-12-13',
+            'end_date': "2014-1-1",
+            'url': "http://test.com",
+            'description': "This is a test description",
+        }, author=self.user)
+        self.assertTrue(form.is_valid())
+        spark = form.save()
+        self.assertEqual(spark.name, "Test Project")
+        self.assertEqual(spark.start_date, datetime.date(1993, 12, 13))
+        self.assertEqual(spark.end_date, datetime.date(2014, 1, 1))
+        self.assertEqual(spark.url, "http://test.com")
+        self.assertEqual(spark.description, "This is a test description")
+        self.assertEqual(spark.category, self.category)
+        self.assertEqual(spark.author, self.user)
+
+    def test_blank_data(self):
+        form = AddSpark({},author=None)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {
+            'name': ['This field is required.'],
+            'category': ['This field is required.'],
+            'start_date': ['This field is required.'],
+            'url': ['This field is required.'],
+        })
+
+    def test_duplicate_spark_names(self):
+        form = AddSpark({
+            'name': 'Test Spark Duplicate',
+            'category': Category.objects.filter(name="Test Category"),
+            'start_date': '1993-1-13',
+            'end_date': "2014-1-12",
+            'url': "http://tttt.com",
+            'description': "This is a test description",
+        }, author=self.user)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {
+            'name': ['Spark with this Name already exists.'],
         })
